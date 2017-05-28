16280e271548ea001be13a63785dfd4d5f94446c
Raquel <raquelibus@hotmail.com>
2017-05-28 17:33:28 +0200

    Add polls app

--- tasks/models.py
+++ tasks/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from spaces.models import Space
 from profiles.models import Profile
 
 
@@ -9,6 +10,7 @@ class Task(models.Model):
     name = models.CharField(max_length=254)
     assign = models.ManyToManyField(Profile, blank=True)
     deadline = models.DateField(null=True, blank=True)
+    space = models.ForeignKey(Space, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
